1eedac54897d1ed58739712e8b30c0ed6de511ad
klinok64 <klinok64@gmail.com>
2025-04-13 12:12:47 +0400

    test revert req

--- installer.py
+++ installer.py
@@ -80,13 +80,13 @@ def setup_accelerate(platform: str) -> None:
 
 def setup_venv(venv_pip):
     subprocess.check_call(
-        f"{venv_pip} install -U torch torchvision --index-url https://download.pytorch.org/whl/cu126",
+        f"{venv_pip} install -U torch==2.1.2 torchvision==0.16.2 --index-url https://download.pytorch.org/whl/cu118",
         shell=PLATFORM == "linux",
     )
     if PLATFORM == "windows":
         subprocess.check_call("venv\\Scripts\\python.exe ..\\fix_torch.py")
     subprocess.check_call(
-        f"{venv_pip} install -U xformers==0.0.29.post3 --index-url https://download.pytorch.org/whl/cu126",
+        f"{venv_pip} install -U xformers==0.0.23.post1 --index-url https://download.pytorch.org/whl/cu118",
         shell=PLATFORM == "linux",
     )
     subprocess.check_call(f"{venv_pip} install -U -r requirements.txt", shell=PLATFORM == "linux")
